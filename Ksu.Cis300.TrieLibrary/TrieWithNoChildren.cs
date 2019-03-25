eeb775e6293d94246164f9d4f254fc9e51861f3a
Taylor Wilcox <tjwilcox@ksu.edu>
2019-03-25 11:59:49 -0500

    Lab 22 Complete.

---
+++ Ksu.Cis300.TrieLibrary/TrieWithNoChildren.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ksu.Cis300.TrieLibrary
+{
+    public class TrieWithNoChildren : ITrie
+    {
+        /// <summary>
+        /// Indicates whether the trie rooted at this node contains the empty string.
+        /// </summary>
+        private bool _hasEmptyString = false;
+
+        /// <summary>
+        /// Adds the given string to this trie.
+        /// </summary>
+        /// <param name="s">The string to add.</param>
+        /// <returns>The resulting trie.</returns>
+        public ITrie Add(string s)
+        {
+            if (s == "")
+            {
+                _hasEmptyString = true;
+                return this;
+            }
+            return new TrieWithOneChild(s, _hasEmptyString);
+        }
+
+        /// <summary>
+        /// Determines whether this trie contains the given string.
+        /// </summary>
+        /// <param name="s">The string to look for.</param>
+        /// <returns>Whether this trie contains s.</returns>
+        public bool Contains(string s)
+        {
+            if (s == "")
+            {
+                return _hasEmptyString;
+            }
+            return false;
+        }
+    }
+}
---
+++ Ksu.Cis300.TrieLibrary/TrieWithOneChild.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ksu.Cis300.TrieLibrary
+{
+    public class TrieWithOneChild : ITrie
+    {
+        /// <summary>
+        /// Indicates whether the trie rooted at this node contains the empty string.
+        /// </summary>
+        private bool _hasEmptyString;
+
+        /// <summary>
+        /// The only child of the Trie.
+        /// </summary>
+        private ITrie _child;
+
+        /// <summary>
+        /// The character label of the only child.
+        /// </summary>
+        private char _label;
+
+        /// <summary>
+        /// Constructor for the TrieWithOneChild.
+        /// </summary>
+        /// <param name="s">The word(string) to be stored.</param>
+        /// <param name="hasEmpty">Whether the empty string is also to be stored.</param>
+        public TrieWithOneChild(string s, bool hasEmpty)
+        {
+            if (s == "" || s[0] < 'a' || s[0] > 'z')
+            {
+                throw new ArgumentException();
+            }
+            _hasEmptyString = hasEmpty;
+            _label = s[0];
+            _child = new TrieWithNoChildren();
+            _child = _child.Add(s.Substring(1));
+
+        }
+
+        /// <summary>
+        /// Adds the given string to the trie rooted at this node.
+        /// </summary>
+        /// <param name="s">The string to add.</param>
+        public ITrie Add(string s)
+        {
+            if (s == "")
+            {
+                _hasEmptyString = true;
+                return this;
+            }
+            else if (s[0] == _label)
+            {
+                _child = _child.Add(s.Substring(1));
+                return this;
+            }
+            else
+            {
+                return new TrieWithManyChildren(s, _hasEmptyString, _label, _child);
+            }
+        }
+
+        /// <summary>
+        /// Gets whether the trie rooted at this node contains the given string.
+        /// </summary>
+        /// <param name="s">The String to look up.</param>
+        /// <returns>Whether the trie rooted at this node contains s.</returns>
+        public bool Contains(string s)
+        {
+            if (s == "")
+            {
+                return _hasEmptyString;
+            }
+            else if (s[0].Equals(_label))
+            {
+                return _child.Contains(s.Substring(1));
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
